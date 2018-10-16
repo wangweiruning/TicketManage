7cc58af3c54792397b9b073c38396bb5f74114a4
lyyy <2289005415@qq.com>
2018-10-16 11:12:35 +0800

    sdfsdg

--- Component/login.js
+++ Component/login.js
@@ -2,9 +2,25 @@ import React from 'react';
 import {View,TouchableOpacity,Text} from 'react-native';
 import {InputItem} from 'antd-mobile-rn';
 import HttpUtils from '../api/Httpdata';
+import MySorage from '../api/storage';
+import {StackActions, NavigationActions} from 'react-navigation';
+
+console.log("++++++++>",NavigationActions);
+
+// const resetAction=(routeName)=>NavigationActions.reset({
+//     index: 0,
+//     actions: [
+//       NavigationActions.navigate({ routeName})
+//     ]
+// });
+const resetAction = StackActions.reset({
+    index: 0,
+    actions: [NavigationActions.navigate({ routeName: 'Tab' })],
+  });
 
 export default class Login extends React.Component{
      constructor(props){
+        MySorage._getStorage()
          super(props)
          this.state={
                     user:'',
@@ -14,23 +30,38 @@ export default class Login extends React.Component{
             }
      }
      
+    // componentDidMount(){
+    //     MySorage.isInit()
+    // }
+
+
      submitgo(url,data){
-         url = url +"?form.user="+data.username+"&form.pass="+data.password+"&code="+'50ACD07A6C49F3B9E082EF40461AC6D1';        HttpUtils.post(url,data)
+        MySorage._sava("isLogin", JSON.stringify({status: true}));
+        MySorage._load("isLogin", (data) => {
+            let res = JSON.parse(data);
+            console.log(res)
+        });
+        // MySorage._sava("dicuserinfo", JSON.stringify(a.data));
+        url = url +"?form.user="+data.username+"&form.pass="+data.password+"&code="+'50ACD07A6C49F3B9E082EF40461AC6D1';  
+        HttpUtils.post(url,data)
         .then(result=>{
+            console.log(result,'qqqqqqqqqq')
             if(data.username!=result.form.user || data.password!=result.form.pass){
                 alert(result.form.targetresult)
             }
             else{
-                alert(result.form.targetresult)
+                alert(":LLLLL"+result.form.targetresult)
+                this.props.navigation.dispatch(resetAction);
             }
             this.setState({
                 result:JSON.stringify(result),//序列化：转换为一个 (字符串)JSON字符串
             });
-            console.log(result,'qqqqqqqqqq')
         })
         .catch(error=> {
+            console.log(error,'aaaaaaaaaa')
             this.setState({
-                result: JSON.stringify(error),//把错误信息格式化为字符串
+                result: JSON.stringify(error),
+            //把错误信息格式化为字符串
             })
         })
      }
