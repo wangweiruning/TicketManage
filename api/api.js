57ac775e5ee788ec085d2c4e1cfa5200d7f0bc07
wangwei <wangweiruning@163.com>
2018-10-18 17:27:42 +0800

    新增接口

--- api/api.js
+++ api/api.js
@@ -61,9 +61,60 @@ export function userlist(data) {
     })
 }
 
+//禁止操作
 export function TicketBasicInfo(data) {
     return Httpdata.post({
         url: url.TicketBasicInfo+data,
+        method: 'post',
+        dataType: "json",
+		contentType: "application/x-www-form-urlencoded",
+    })
+}
+
+//查询当前两票的基本信息
+export function searchTicketBasicInfo(data) {
+    return Httpdata.post({
+        url: url.searchTicketBasicInfo+data,
+        method: 'post',
+        dataType: "json",
+		contentType: "application/x-www-form-urlencoded",
+    })
+}
+
+//查询当前两票流程
+export function searchTicketFlow(data) {
+    return Httpdata.post({
+        url: url.searchTicketFlow+data,
+        method: 'post',
+        dataType: "json",
+		contentType: "application/x-www-form-urlencoded",
+    })
+}
+
+//验证当前类型流程的创建权限中是否包含该用户
+export function searchUserPower(data) {
+    return Httpdata.post({
+        url: url.searchUserPower+data,
+        method: 'post',
+        dataType: "json",
+		contentType: "application/x-www-form-urlencoded",
+    })
+}
+
+//查询流转目标
+export function searchUserForRole(data) {
+    return Httpdata.post({
+        url: url.searchUserForRole+data,
+        method: 'post',
+        dataType: "json",
+		contentType: "application/x-www-form-urlencoded",
+    })
+}
+
+//新建两票时生成两票编号
+export function newTiceketNum(data) {
+    return Httpdata.post({
+        url: url.newTiceketNum+data,
         method: 'post',
         dataType: "json",
 		contentType: "application/x-www-form-urlencoded",
--- api/serviceAPI.config.js
+++ api/serviceAPI.config.js
@@ -7,5 +7,11 @@ const URL ={
     editquanxian:url+'ticketMng/ticketMng_searchEditPara.action',//编辑模板权限管理
     userlist:url+'home/home_showUserDatas.action',//用户信息
     TicketBasicInfo:url+'baseInformation/templateMng_templateContentSearch.action', //模板信息
+    searchTicketBasicInfo:url+'ticketMng/ticketMng_searchTicketBasicInfo.action',//查询当前两票的基本信息
+    searchTicketFlow:url+'ticketMng/ticketMng_searchTicketFlow.action',// 查询当前两票流程
+    searchUserPower:url+'ticketMng/ticketMng_searchUserPower.action.action',// 验证当前类型流程的创建权限中是否包含该用户
+    searchUserForRole:url+'ticketMng/ticketMng_searchUserForRole.action',// 查询流转目标
+    newTiceketNum:url+'ticketMng/ticketMng_newTiceketNum.action',// 新建两票时生成两票编号
+    
 }
 export default URL
