5b792b2c1b8013816b5568ef6a63b306b814c4da
lyyy <2289005415@qq.com>
2018-10-25 16:23:46 +0800

    sdfdf

--- Component/TicketDetail.js
+++ Component/TicketDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Title from './Title';
 import {InputItem,DatePicker,List,Checkbox,TextareaItem, Button} from 'antd-mobile-rn';
-import {View,Text,ScrollView,TouchableOpacity,Picker,ToastAndroid,Modal} from 'react-native';
+import {View,Text,ScrollView,TouchableOpacity,Picker,ToastAndroid,Modal,DatePickerAndroid} from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 import {newTiceketNum,searchTicketBasicInfo,TicketBasicInfo,searchTicketFlow,editquanxian,searchUserForRole} from './../api/api'
 import DropdownCheckbox from './DropdownCheckbox';
@@ -111,6 +111,7 @@ export default class Tdetail extends React.Component{
         this.setState({
             [k]: v
         });
+        alert(v)
     }
 
 
@@ -147,19 +148,18 @@ export default class Tdetail extends React.Component{
                   <Text style={{color:'#3e5ed2',left:5}}>{v.ParaName}</Text>
               </View>
                {      
-                  v.ParaTypeID==4? <DropdownCheckbox style={{backgroundColor:'white',height:50}} TextColor={{color:'black',fontSize:13}} SelectData={this.state.user}/>:                   v.ParaTypeID==3?<Picker style={{ height: 50, width:'100%'}} enabled={!dis} mode='dropdown' selectedValue={this.state.language}
+                  v.ParaTypeID==4? <DropdownCheckbox style={{backgroundColor:'white',height:50}} TextColor={{color:'black',fontSize:13}} SelectData={this.state.user}/>:v.ParaTypeID==3?<Picker style={{ height: 50, width:'100%'}} enabled={!dis} mode='dropdown' selectedValue={this.state.language}
                    onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
                    <Picker.Item label="Java" value="java" />
                    <Picker.Item label="JavaScript" value="js" />
                    </Picker>:v.ParaTypeID==2?<InputItem editable={dis} onChange={(v)=>this.handleInput('username',v)} style={{borderRadius:5,backgroundColor:'white',width:'85%',backgroundColor:"#fffeee"}}/>:v.ParaTypeID==5?<View style={{left:5,width:290}}>
-                   <DatePicker
+                   <DatePicker 
                      value={this.state.value}
                      mode="date"
-                     minDate={new Date(1999, 7, 6)}
-                     maxDate={new Date(2000, 11, 3)}
+                     minDate={new Date(2015,1,1)}
                      onChange={this.onChange}
                      format="YYYY-MM-DD"
-                     disabled={dis}
+                    //  disabled={dis}
                    >
                     <List.Item arrow="horizontal"></List.Item>
                     </DatePicker></View>:v.ParaTypeID==6?
@@ -184,24 +184,22 @@ export default class Tdetail extends React.Component{
                   <Text style={{color:'#3e5ed2',left:5}}>提交</Text>
               </View>
             <View style={{display:'flex',justifyContent:'center',margin:5}}>
-                <Text>是否同意</Text>
-                
+                <Text style={{color:'black'}}>是否同意</Text>
                 <ModalDropdown 
                 dropdownStyle={{width:'100%'}} textStyle={{color:'black',fontSize:13,left:5}} 
                 style={{backgroundColor:'skyblue',width:'100%',height:29.3,justifyContent:'center'}} defaultValue={'请选择'} options={['同意', '拒绝']}/>
             </View>
             <View style={{display:'flex',justifyContent:'center',margin:5}}>
-                <Text>流转状态</Text>
+                <Text style={{color:'black'}}>流转状态</Text>
                 {
                   this.state.status!="" &&
                   <ModalDropdown 
-                  
                   dropdownStyle={{width:'100%'}} textStyle={{color:'black',fontSize:13,left:5}} 
                   style={{backgroundColor:'skyblue',width:'100%',height:29.3,justifyContent:'center'}} defaultValue={'请选择'} options={this.state.status}/>
                 }
                 </View>
             <View style={{display:'flex',justifyContent:'center',margin:5}}>
-                <Text>流转目标</Text>
+                <Text style={{color:'black'}}>流转目标</Text>
                 
                 <DropdownCheckbox style={{backgroundColor:'skyblue'}} TextColor={{color:'black',fontSize:13}} SelectData={this.state.user}/>
             </View>
