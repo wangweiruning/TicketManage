0f3695e626c24b2c16ffe4bee678401af72e1510
lyyy <2289005415@qq.com>
2018-11-29 16:15:22 +0800

    safafafsasf

--- Component/TicketDetail.js
+++ Component/TicketDetail.js
@@ -631,8 +631,8 @@ export default class Tdetail extends React.Component{
                                  <TextareaItem placeholderTextColor="#eee" editable={!dis} placeholder="请输入内容..."
                       onChange={(e)=>this.handleInput('datalist'+i,e,getAllTempanyId[i])}
                        autoHeight
-                         style={{fontSize:14,paddingHorizontal:6,minWidth:'98%',backgroundColor:!dis?"rgba(255,255,255,.2)":"rgba(255,255,255,.4)"}} />
-                         <View style={{flexDirection:'row',backgroundColor:'rgba(255,255,255,.4)',padding:5}}>
+                         style={{color:'white',fontSize:14,paddingHorizontal:6,minWidth:'98%',backgroundColor:!dis?"rgba(255,255,255,.2)":"rgba(255,255,255,.4)"}} />
+                         <View style={{flexDirection:'row',backgroundColor:!dis?"rgba(255,255,255,.2)":"rgba(255,255,255,.4)",padding:5}}>
                        <Checkbox onChange={(e)=>this.onChangecoform(getAllTempanyId[i]+"_1",e.target.checked)} disabled={dis}>
                           <Text style={{color:'#f5f5f5'}}>是否已执行</Text></Checkbox></View></View>
                         }
